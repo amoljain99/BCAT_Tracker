957b68decb133aaf6a284dba04477ca615c803f4
Vikram Pote <amoljain108@gmail.com>
2018-04-30 20:16:19 +0530

    Added POC For SqliteDemo

---
+++ BCAT_Tracker.iOS/Views/CEDetailViewController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using UIKit;
+
+namespace BCAT_Tracker.iOS.Views
+{
+    public partial class CEDetailViewController : UIViewController
+    {
+        public CEDetailViewController() : base("CEDetailViewController", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
---
+++ BCAT_Tracker.iOS/Views/CETableViewController.designer.cs
@@ -0,0 +1,21 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace BCAT_Tracker.iOS.Views
+{
+    [Register ("CETableViewController")]
+    partial class CETableViewController
+    {
+        void ReleaseDesignerOutlets ()
+        {
+        }
+    }
+}
